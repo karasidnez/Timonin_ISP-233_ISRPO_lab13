bad80e9eb9d61cc80a56d7f0f6599a6e61df1e6e
karasidnez <karasdnez@gmail.com>
2025-11-10 07:23:14 +0300

    Add files via upload

---
+++ Lab13_Wpf_Demo/Wpf_Demo/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Demo;
+
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        //Button myButton = new Button();
+        //myButton.Content = "B";
+
+        //Grid.SetRow(myButton, 3);
+        //Grid.SetColumn(myButton, 3);
+
+        //Grid myGrid = (Grid)FindName("myGrid");
+
+        //myGrid.Children.Add(myButton);
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e) {
+        MessageBox.Show("OLEEEg");
+    }
+}
